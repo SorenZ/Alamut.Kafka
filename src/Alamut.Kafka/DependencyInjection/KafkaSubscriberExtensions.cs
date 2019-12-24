e6947e3021abf26a730eed1dbcbb6fb28cca28a7
Hamed ZVand <sunrise.freedom@gmail.com>
2019-12-24 10:33:06 +0330

    make document for AddNewHostedSubscriber

--- src/Alamut.Kafka/DependencyInjection/KafkaSubscriberExtensions.cs
+++ src/Alamut.Kafka/DependencyInjection/KafkaSubscriberExtensions.cs
@@ -1,6 +1,8 @@
 using System.Linq;
+
 using Alamut.Kafka.Models;
 using Alamut.Kafka.SubscriberHandlers;
+
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
@@ -9,6 +11,13 @@ namespace Alamut.Kafka
 {
     public static class KafkaSubscriberExtensions
     {
+        /// <summary>
+        /// adds a new Kafka subscriber as HostedService (long running back-ground service)
+        /// </summary>
+        /// <param name="services"></param>
+        /// <param name="groupId">group Id to subscribes with it, if it's not provided use groupId in KafkaConfig</param>
+        /// <param name="topics">topics to subscribes, if it's not provided subscribes to all topics in KafkaConfig</param>
+        /// <returns></returns>
         public static IServiceCollection AddNewHostedSubscriber(this IServiceCollection services, 
         string groupId = null, 
         params string[] topics)
